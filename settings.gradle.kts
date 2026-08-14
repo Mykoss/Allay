b3d9c9303157499f159f888d62ae8d8c7036fc79
Mykoss <mykoss022@proton.me>
2026-08-14 07:34:26 -0500

    Prevent stale Protocol builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,19 @@ include(":server")
 include(":codegen")
 include(":data")
 
+val requiredProtocolRegressionTest = file(
+    "protocol-local/bedrock-codec/src/test/java/org/cloudburstmc/protocol/bedrock/codec/" +
+            "v2168/serializer/ItemStackResponseSerializer_v2168Test.java"
+)
+check(requiredProtocolRegressionTest.isFile) {
+    """
+    protocol-local is missing the required v2168 inventory framing fix.
+    Run these commands before building:
+      git submodule sync --recursive
+      git submodule update --init --recursive
+    """.trimIndent()
+}
+
 includeBuild("protocol-local") {
     dependencySubstitution {
         substitute(module("org.allaymc.protocol:bedrock-codec"))
