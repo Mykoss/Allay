7d815c9229f3106f94ba861405de3eae6997bbc9
Mykoss <mykoss022@proton.me>
2026-08-17 18:45:08 -0500

    Fix NPC entity interaction handling

--- server/src/main/java/org/allaymc/server/network/processor/ingame/InteractPacketProcessor.java
+++ server/src/main/java/org/allaymc/server/network/processor/ingame/InteractPacketProcessor.java
@@ -1,7 +1,10 @@
 package org.allaymc.server.network.processor.ingame;
 
 import org.allaymc.api.container.ContainerTypes;
+import org.allaymc.api.entity.interfaces.EntityNpc;
+import org.allaymc.api.eventbus.event.player.PlayerInteractEntityEvent;
 import org.allaymc.api.player.Player;
+import org.allaymc.server.network.NetworkHelper;
 import org.allaymc.server.network.processor.PacketProcessor;
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacketType;
 import org.cloudburstmc.protocol.bedrock.packet.InteractPacket;
@@ -14,6 +17,32 @@ public class InteractPacketProcessor extends PacketProcessor<InteractPacket> {
     public void handleSync(Player player, InteractPacket packet, long receiveTime) {
         if (packet.getAction() == InteractPacket.Action.OPEN_INVENTORY) {
             player.getControlledEntity().getContainer(ContainerTypes.INVENTORY).addViewer(player);
+            return;
+        }
+
+        if (packet.getAction() != InteractPacket.Action.NPC_OPEN) {
+            return;
+        }
+
+        var entity = player.getControlledEntity();
+        var target = entity.getDimension().getEntityManager().getEntity(packet.getRuntimeEntityId());
+        if (!(target instanceof EntityNpc) || !entity.canReach(target.getLocation())) {
+            return;
+        }
+
+        var itemInHand = entity.getItemInHand();
+        var event = new PlayerInteractEntityEvent(
+                entity,
+                target,
+                itemInHand,
+                NetworkHelper.fromNetwork(packet.getMousePosition())
+        );
+        if (!event.call()) {
+            return;
+        }
+
+        if (!itemInHand.interactEntity(entity, target)) {
+            target.onInteract(entity, itemInHand);
         }
     }
 
